c116a64a7701c092b02c3d7733c5cf1536817749
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-30 11:47:25 -0300

    Criando Footer

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Header from './components/Header'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 export default function RoutesApp() {
     return (
@@ -10,6 +11,7 @@ export default function RoutesApp() {
             <Routes>
                 <Route path='/' element={<Home/>}/>
             </Routes>
+            <Footer/>
         </BrowserRouter>
     )
 }
