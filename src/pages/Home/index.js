b63c2f1618ac624b21287b5c41513962762aefa0
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-30 11:29:56 -0300

    Criando página Home e pasta de imagens

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import './Home.css'
+
+import AyrtonSenna from '../../images/ayrton-senna.png'
+
+export default function Home() {
+    return (
+        <div>
+            <div className='img'>
+                <img src={AyrtonSenna} alt='Ayrton Senna'/>
+            </div>
+            <section>
+                <p>Ayrton Senna da Silva (São Paulo, 21 de março de 1960 — Bolonha, 1 de maio de 1994) foi um piloto brasileiro de Fórmula 1, campeão da categoria três vezes, em 1988, 1990 e 1991. Ele começou sua carreira competindo no kart em 1973 e em "carros de fórmula" em 1981, quando venceu as Fórmulas Ford 1600 e 2000. Em 1983 alcançou o título de campeão do Campeonato Britânico de Fórmula 3 batendo vários recordes. Seu desempenho impulsionou sua ascensão à Fórmula 1, fazendo sua primeira aparição na categoria no Grande Prêmio do Brasil de 1984 pela equipe Toleman-Hart. Em sua primeira temporada, Senna pontuou em cinco corridas, fechando o ano com treze pontos e a nona posição na classificação geral dos pilotos. No ano seguinte, ingressou na Lotus-Renault, pela qual venceu seis grandes prêmios ao longo de três temporadas.</p>
+                <p>Em 1988, juntou-se ao francês Alain Prost na McLaren-Honda, com o qual teve grande rivalidade. Senna venceu oito etapas daquela temporada e sagrou-se campeão mundial pela primeira vez. Após a polêmica final de 1989 com Prost que resultou na segunda colocação do torneio, ele retomou o título em 1990, vencendo novamente na temporada seguinte, tornando-se o piloto mais jovem a conquistar um tricampeonato na Fórmula 1 até então. Em 1993, Senna foi vice-campeão, vencendo cinco corridas. Transferiu-se para a Williams em 1994, onde disputou apenas três etapas, a última sendo o Grande Prêmio de San Marino, onde se acidentou e faleceu. Ao todo, Senna participou de 161 grandes prêmios na Fórmula 1, alcançando 41 vitórias, 80 pódios, 65 pole positions e 19 voltas mais rápidas.</p>
+                <p>Além das corridas de carros, Senna dedicava-se a jet skis, motos, aeromodelos e principalmente helicópteros. Ele também administrava diversas marcas e empreendimentos, além de ter patrocinado vários programas de assistência filantrópica, principalmente os ligados a crianças carentes. Depois de morrer, sua irmã, Viviane Senna, fundou o Instituto Ayrton Senna, uma organização não governamental que oferece oportunidades de desenvolvimento humano a crianças e jovens de baixa renda. Além disso, o personagem Senninha foi criado com a intenção de atingir o público infantil com os ideais do piloto, como a superação, dedicação e o gosto pela vitória.</p>
+                <p>Sua morte, assim como o funeral e velório, provocou uma das maiores comoções da história do Brasil, bem como repercussão mundial. Em pesquisas feitas com jornalistas especializados, pilotos e torcedores, foi amplamente considerado o melhor piloto da história da Fórmula 1 e um dos maiores automobilistas de todos os tempos. Em 1999, foi eleito pela revista "Isto É" o esportista do século XX no Brasil. No auge de sua carreira, era considerado o maior ídolo brasileiro, posto que mantém mesmo depois de quase três décadas após a sua morte.</p>
+            </section>
+        </div>
+    )
+}
