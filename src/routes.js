591d2b8d16c738a92e85299d464de468d8dec6b9
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-30 12:06:04 -0300

    Criando a página Condecorações

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Recordes from './pages/Recordes'
+import Condecoracoes from './pages/Condecoracoes'
 import Footer from './components/Footer'
 
 export default function RoutesApp() {
@@ -12,6 +13,7 @@ export default function RoutesApp() {
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/recordes' element={<Recordes/>}/>
+                <Route path='/condecoracoes' element={<Condecoracoes/>}/>
             </Routes>
             <Footer/>
         </BrowserRouter>
