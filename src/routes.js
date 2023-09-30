448598b24851067c7571f491b3d425d9bbbd7d67
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-30 11:59:47 -0300

    Criando a página Recordes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Header from './components/Header'
 import Home from './pages/Home'
+import Recordes from './pages/Recordes'
 import Footer from './components/Footer'
 
 export default function RoutesApp() {
@@ -10,6 +11,7 @@ export default function RoutesApp() {
             <Header/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/recordes' element={<Recordes/>}/>
             </Routes>
             <Footer/>
         </BrowserRouter>
